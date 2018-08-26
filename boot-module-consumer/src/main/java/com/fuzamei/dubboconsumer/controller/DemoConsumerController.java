2827682094d9bf3916e5774e3e3434b6e06e6a67
杨凌霄 <ylx@disanbo.com>
2018-08-26 16:45:12 +0800

    new

--- boot-module-consumer/src/main/java/com/fuzamei/dubboconsumer/controller/DemoConsumerController.java
+++ boot-module-consumer/src/main/java/com/fuzamei/dubboconsumer/controller/DemoConsumerController.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.fuzamei.dubboapi.consumer.DemoService;
 import com.fuzamei.dubboapi.pojo.TestBO;
 import com.fuzamei.dubboapi.pojo.UserDO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
